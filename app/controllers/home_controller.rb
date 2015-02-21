6556bdd5515e9c011c624ef72edb10b30bfc08cb
jangelroa <jangelroa@gmail.com>
2015-02-21 13:42:45 -0800

    add gem devise, gem bootstrap, create logout on index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @message = "Welcome to my website!"
+    if user_signed_in?
+      @message = "Welcome to my website! #{current_user.email}"
+      @email = current_user.email
+    else
+      @message = "NOT signed in!"
+    end
   end
 end
---
+++ test/models/use_rrake_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UseRrakeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
